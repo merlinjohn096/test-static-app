b53581498dd34e24547794a3acc2105cd2d8f3c8
Merlin John <v-merljo@microsoft.com>
2024-02-25 00:32:28 +0530

    update

--- my-app/api/message/index.js
+++ my-app/api/message/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     const name = process.env.REACT_APP_NAME;
     context.res.json({
-        text: `Hello ${name}`
+        text: `Hai ${name}`
     });
 };
